5baf119ba00d5c621a5bce29624b1078963cb40e
Glitch (ride-share) <none>
2018-01-03 23:50:03 +0000

    🎡💇 Checkpoint bot.js:616718

--- bot.js
+++ bot.js
@@ -143,7 +143,8 @@ if (!process.env.clientId || !process.env.clientSecret) {
   // controller.studio.before, controller.studio.after and controller.studio.validate
   if (process.env.studio_token) {
       controller.hears(['info'], 'direct_message,direct_mention,mention', function(bot, message) {
-                  controller.storage.users.save({id: message.user, name:'Bob'}, function(err, user) {bot.reply(message, 'saved');});
+                  bot.reply(message, message.user);
+                  //controller.storage.users.save({id: message.user, name:'Jo'}, function(err, user) {bot.reply(message, 'saved');});
                   controller.storage.users.get(message.user, function(err, user) {bot.reply(message, 'ok' + user.name);});
                   //bot.reply(message, 'ok' + data);
                   // no trigger was matched
