7a7ecdbec902a451476f5640418963bb3a98c8c1
Glitch (ride-share) <none>
2018-01-03 23:01:02 +0000

    📉🏪 Checkpoint

--- skills/sample_conversations.js
+++ skills/sample_conversations.js
@@ -20,7 +20,7 @@ module.exports = function(controller) {
                 convo.say('Cool, let\'s start at ' + response.text);
                 convo.next();
                 convo.ask('Where will you drive to?', function(response, convo) {
-                  convo.reply('Great, let\s go to ' + response.text);
+                  convo.say('Great, let\s go to ' + response.text);
                 });
 
             });
