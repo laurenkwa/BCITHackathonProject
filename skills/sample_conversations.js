6a14eb02539f4c5f1a7731c7abc7c4b98a3da2cc
Glitch (ride-share) <none>
2018-01-03 23:11:02 +0000

    😎🚙 Checkpoint

--- skills/sample_conversations.js
+++ skills/sample_conversations.js
@@ -24,7 +24,7 @@ module.exports = function(controller) {
                   convo.next();
                   convo.say('Great, let\s go to ' + destination);
                   convo.say('Here\'s your map.');
-                  convo.say('"https://www.google.com/maps/embed/v1/directions?origin=" + origin + "&destination=" + destination +   "&key=AIzaSyAh-wxnCsW7OZsqkWMHXLFtdjwLXo1PsqY"
+                  convo.say("https://www.google.com/maps/embed/v1/directions?origin=" + origin + "&destination=" + destination +   "&key=AIzaSyAh-wxnCsW7OZsqkWMHXLFtdjwLXo1PsqY");
                 });
 
             });
