367f4238ae23f8c677d7baf65e732c0db785e4cc
Glitch (ride-share) <none>
2018-01-03 23:58:03 +0000

    🚀⏰ Checkpoint bot.js:616718

--- bot.js
+++ bot.js
@@ -148,7 +148,7 @@ if (!process.env.clientId || !process.env.clientSecret) {
       });
       controller.hears(['get'], 'direct_message,direct_mention,mention', function(bot, message) {
         bot.reply(message, message.channel);
-        controller.storage.users.get(message.channel, function(err, user) {bot.reply(message, 'ok' + user.name);});   
+        controller.storage.users.get(message.channel, function(err, user) {bot.reply(message, user.name);});   
       });
   } else {
       console.log('~~~~~~~~~~');
