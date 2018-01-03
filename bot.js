a61001ae2abb74b3d975f3dfda32e4d1d464999f
Glitch (ride-share) <none>
2018-01-03 22:36:02 +0000

    🏦👅 Checkpoint bot.js:616740

--- bot.js
+++ bot.js
@@ -148,6 +148,7 @@ if (!process.env.clientId || !process.env.clientSecret) {
       controller.on('direct_message,direct_mention,mention', function(bot, message) {
           controller.studio.runTrigger(bot, message.text, message.user, message.channel, message).then(function(convo) {
               if (!convo) {
+                  controller.storage.users.save({id: message.user, foo:'bar'}, function(err) {});
                   bot.reply(message,'OK');
                   // no trigger was matched
                   // If you want your bot to respond to every message,
