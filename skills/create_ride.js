69796d310cdef4f4fc73bdf43e4e6566575fd021
Glitch (ride-share) <none>
2018-01-04 05:51:10 +0000

    👊🌌 Checkpoint

--- skills/create_ride.js
+++ skills/create_ride.js
@@ -81,10 +81,11 @@ module.exports = function(controller) {
                       var correct = response.text;
                         if (correct.toUpperCase() == 'YES') {
                           convo.say('Great. I\'ll tell the rideshare channel.');
-                          controller.storage.channels.delete(message.user, function(err){});
-                                  controller.storage.channels.save({id: message.user, name:message.text}, function(err, user) {bot.reply(message, 'saved');});
-                          controller.storage.channels.save({id: message.user, name: name, image: thumbnail}, function(err, user) {});
-                          controller.storage.channels.get(message.user, function(err, user) {bot.reply(message, 'New route created: ' + user.name);});  
+                          //controller.storage.channels.delete(message.user, function(err){});
+                          controller.storage.channels.save({id: message.user, name:message.text}, function(err, user) {bot.reply(message, 'saved');});
+                          //controller.storage.channels.save({id: message.user, name: name, image: thumbnail}, function(err, user) {});
+                          //controller.storage.channels.get(message.user, function(err, user) {bot.reply(message, 'New route created: ' + user.name);});  
+                          controller.storage.channels.get(message.user, function(err, user) {bot.reply(message, 'ok' + user.name);});  
                           //convo.next();
                           alertChannel(bot, submission);
                         } else {
