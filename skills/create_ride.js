7c11e73f85bd5237cecd54abb061af1adaf7f7da
Glitch (ride-share) <none>
2018-01-04 19:56:41 +0000

    🙆🏬 Checkpoint skills/create_ride.js:616718

--- skills/create_ride.js
+++ skills/create_ride.js
@@ -60,6 +60,14 @@ var selectionButton = {
 }
 
 module.exports = function(controller) {
+  
+    controller.hears(['private'], 'direct_message,direct_mention', function(bot, message) {
+        bot.say(bot.api.conversations.open({
+          token: process.env.legacyToken,
+          users: 'U5E31FZAB',
+        }).channel.id);
+    });
+  
   controller.on('slash_command', function(bot, message) {
         bot.replyAcknowledge();
         bot.reply({text: '', channel: message.user}, selectionButton);
@@ -284,7 +292,7 @@ module.exports = function(controller) {
                           //convo.next();
                           controller.storage.channels.delete(message.user, function(err){
                             var time = (parseInt(submission.Time) <= 12) 
-                              ? ((submission.Time == '00') ? '12:00' : submission.Time) + ':00 am' 
+                              ? ((submission.Time == '00') ? '12' : submission.Time) + ':00 am' 
                               : (parseInt(submission.Time) - 12) + ':00 pm';
                             controller.storage.channels.save({id: message.user, name: name, image: thumbnail, driver: '<@' + message.user + '>', seats:submission.Seats, time: time, twentyFourTime:submission.Time, date:submission.Date}, function(err, user) {
                               controller.storage.channels.get(message.user, function(err, user) {bot.reply(message, 'New route created: ' + user.name); alertChannel(bot, user);});  
@@ -310,12 +318,7 @@ module.exports = function(controller) {
         });
       }
   
-    controller.hears(['private'], 'direct_message,direct_mention', function(bot, message) {
-        bot.say(bot.api.conversations.open({
-          token: process.env.legacyToken,
-          users: 'U5E31FZAB',
-        }).channel.id);
-    });
+
     
   
 
