fe7213a83337bd09cf71da9cc18e1df7b225b1a0
Glitch (ride-share) <none>
2018-01-04 18:36:38 +0000

    🚏🍲 Checkpoint

--- skills/create_ride.js
+++ skills/create_ride.js
@@ -34,7 +34,29 @@ module.exports = function(controller) {
               .addText('Seats','Seats','Number of Seats Available')
               .addText('Date', 'Date', 'Date')
               .addSelect('Select','select',null,[
-                {label:'12:00 am',value:'00'}, 
+                {label:'12:00 am',value:'00'},
+                {label:'1:00 am',value:'00'},
+                {label:'2:00 am',value:'00'},
+                {label:'3:00 am',value:'00'},
+                {label:'4:00 am',value:'00'},
+                {label:'5:00 am',value:'00'},
+                {label:'6:00 am',value:'00'},
+                {label:'7:00 am',value:'00'},
+                {label:'8:00 am',value:'00'},
+                {label:'9:00 am',value:'00'},
+                {label:'10:00 am',value:'00'},
+                {label:'11:00 am',value:'00'},
+                {label:'12:00 am',value:'00'},
+                {label:'12:00 pm',value:'00'},
+                {label:'12:00 pm',value:'00'},
+                {label:'12:00 pm',value:'00'},
+                {label:'12:00 pm',value:'00'},
+                {label:'12:00 pm',value:'00'},
+                {label:'12:00 pm',value:'00'},
+                {label:'12:00 pm',value:'00'},
+                {label:'12:00 pm',value:'00'},
+                {label:'12:00 pm',value:'00'},
+                {label:'12:00 pm',value:'00'}, 
                 {label:'11:00 pm',value:'23'}
               ], {placeholder: 'Select One'});
               //.addText('Time of Departure','Time','0:00 - 24:00');
