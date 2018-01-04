b5e56f3f0f4c771812a121652e919774a0e8eb4f
Glitch (ride-share) <none>
2018-01-04 06:53:12 +0000

    ✂️🎣 Checkpoint

--- bot.js
+++ bot.js
@@ -220,9 +220,9 @@ if (!process.env.clientId || !process.env.clientSecret) {
             controller.storage.channels.all(function(err, user) {
               for(var i = 0; i < user.length; i++){
                 if(user[i].id == message.user){
-                  bot.reply(message, 'works');
+                  //bot.reply(message, 'works');
                   bot.reply({text: '', channel: message.actions[0].value}, 'You have been accepted to car pool on the ' + user[i].name + ' route');
-                  startPrivateMessage();
+                  startPrivateMessage(message);
                 }
               }
             }); 
