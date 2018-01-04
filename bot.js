d7f3b510a42597b70418491c83c0db5d7ed9361f
Glitch (ride-share) <none>
2018-01-04 02:19:06 +0000

    👩‍❤️‍💋‍👩📷 Checkpoint bot.js:616718

--- bot.js
+++ bot.js
@@ -207,7 +207,7 @@ if (!process.env.clientId || !process.env.clientSecret) {
   if (process.env.studio_token) {
       controller.on('interactive_message_callback', function(bot, message) {
         if(message.callback_id == 'route_selection'){
-         bot.reply(message, 'you chose a route!'); 
+         bot.reply(message, 'You chose a route!'); 
         }
       });
       controller.hears(['game'], 'direct_message,direct_mention,mention', function(bot, message) {
