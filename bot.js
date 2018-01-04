6366698e6f704d52b4a494f371a47f5db9b5c4ea
Glitch (ride-share) <none>
2018-01-04 08:26:14 +0000

    🎢💰 Checkpoint

--- bot.js
+++ bot.js
@@ -216,7 +216,7 @@ if (!process.env.clientId || !process.env.clientSecret) {
       controller.on('interactive_message_callback', function(bot, message) {
         if(message.callback_id == 'accept_ride_request'){
           if(message.actions[0].name == "Yes"){
-            bot.reply(message, 'Rider Accepted' + message.actions[0].value); 
+            bot.reply(message, 'Rider Accepted'); 
             controller.storage.channels.all(function(err, user) {
               for(var i = 0; i < user.length; i++){
                 if(user[i].id == message.user){
