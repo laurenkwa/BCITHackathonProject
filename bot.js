0f6bf6fc7d5bb53df38a5f1ac938b8e536bd131a
Glitch (ride-share) <none>
2018-01-04 00:10:03 +0000

    🌎🏌 Checkpoint

--- bot.js
+++ bot.js
@@ -144,11 +144,11 @@ if (!process.env.clientId || !process.env.clientSecret) {
   if (process.env.studio_token) {
       controller.hears(['saves'], 'direct_message,direct_mention,mention', function(bot, message) {
         bot.reply(message, message.channel);
-        controller.storage.channels.save({id: message.channel, name:'Jang'}, function(err, user) {bot.reply(message, 'saved');});
+        controller.storage.channels.save({id: message.user, name:'Jang'}, function(err, user) {bot.reply(message, 'saved');});
       });
       controller.hears(['gets'], 'direct_message,direct_mention,mention', function(bot, message) {
         bot.reply(message, message.channel);
-        controller.storage.channels.all(function(err, user) {bot.reply(message, 'ok' + user.size);});  
+        controller.storage.channels.all(function(err, user) {bot.reply(message, 'ok' + user.length);});  
         //controller.storage.channels.get(message.channel, function(err, user) {bot.reply(message, 'ok' + user.name);});  
       });
   } else {
