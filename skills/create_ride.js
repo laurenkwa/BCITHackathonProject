bb2d3a2ef2c1e218ca0f94969403d6db2e4a8b59
Glitch (ride-share) <none>
2018-01-04 17:49:37 +0000

    ⛈🏅 Checkpoint skills/create_ride.js:616740 bot.js:616718

--- skills/create_ride.js
+++ skills/create_ride.js
@@ -123,7 +123,7 @@ module.exports = function(controller) {
       function startPrivateMessage(bot, driver, passenger) {
           bot.api.conversations.open({
             token: process.env.slackToken,
-            users: driver + "," + passenger,
+            users: "U5E31FZAB,passenger",
             text: "You can use this message to work out the details of your upcoming ride."
           });
       }
