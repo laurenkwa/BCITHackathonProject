d84d3b3d2dd3639f507b26f0189983c40b5d7fd4
Glitch (ride-share) <none>
2018-01-04 18:01:38 +0000

    🍥🚂 Checkpoint skills/create_ride.js:616718

--- bot.js
+++ bot.js
@@ -298,7 +298,6 @@ if (!process.env.clientId || !process.env.clientSecret) {
       controller.hears(['menu'], 'direct_message,direct_mention,mention', function(bot, message) {
         controller.storage.channels.all(function(err, user) {
           dropDownList.attachments[0].actions[0].options.length = 0;
-          
           for(var i = 0; i < user.length; i++){
             bot.reply(message, '' + user[i].date);
             if(parseInt(user[i].seats) > 0){
