1927fe4263cde8bce9e862d97e0b02bbcfb74388
Glitch (ride-share) <none>
2018-01-04 00:49:04 +0000

    📹🔖 Checkpoint

--- skills/sample_conversations.js
+++ skills/sample_conversations.js
@@ -27,7 +27,8 @@ module.exports = function(controller) {
                     var jsonMap = "https://maps.googleapis.com/maps/api/directions/json?origin=" + origin.replace(/ /g, "%20") + "&destination=" + destination.replace(/ /g, "%20") + "&mode=driving&key=AIzaSyAh-wxnCsW7OZsqkWMHXLFtdjwLXo1PsqY";
                     var mapObject = JSON.parse(jsonMap);
                     var route = mapObject.routes[0];
-                    convo.ask(
+                    var ->overview_polyline->points
+                    convo.say(route);
                     convo.ask('Does this look correct?', function (response, convo) {
                       convo.next();
                       if (response.text == 'no' || response.text == 'No' || response.text == 'NO') {
