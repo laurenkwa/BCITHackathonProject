c7386d7ef01570ab019351bb4762ce85cc7ead1d
Glitch (ride-share) <none>
2018-01-04 19:09:40 +0000

    🐓🚈 Checkpoint

--- skills/create_ride.js
+++ skills/create_ride.js
@@ -91,7 +91,32 @@ module.exports = function(controller) {
               'Route Selector',
               'callback_id',
               'Submit'
-            ).addText('Your City','riderLocation','Your Address')
+            ).addSelect('Select Your City','city',null,[
+                {label:'12:00 am',value:'00'},
+                {label:'1:00 am',value:'1'},
+                {label:'2:00 am',value:'2'},
+                {label:'3:00 am',value:'3'},
+                {label:'4:00 am',value:'4'},
+                {label:'5:00 am',value:'5'},
+                {label:'6:00 am',value:'6'},
+                {label:'7:00 am',value:'7'},
+                {label:'8:00 am',value:'8'},
+                {label:'9:00 am',value:'9'},
+                {label:'10:00 am',value:'10'},
+                {label:'11:00 am',value:'11'},
+                {label:'12:00 am',value:'12'},
+                {label:'1:00 pm',value:'13'},
+                {label:'2:00 pm',value:'14'},
+                {label:'3:00 pm',value:'15'},
+                {label:'4:00 pm',value:'16'},
+                {label:'5:00 pm',value:'17'},
+                {label:'6:00 pm',value:'18'},
+                {label:'7:00 pm',value:'19'},
+                {label:'8:00 pm',value:'20'},
+                {label:'9:00 pm',value:'21'},
+                {label:'10:00 pm',value:'22'}, 
+                {label:'11:00 pm',value:'23'}
+              ], {placeholder: 'Select One'})
               .addSelect('Select a Earliest Time','time',null,[
                 {label:'12:00 am',value:'00'},
                 {label:'1:00 am',value:'1'},
