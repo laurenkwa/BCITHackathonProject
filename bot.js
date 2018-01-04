2a61c7e7817715458f21465a4e1c41749376263b
Glitch (ride-share) <none>
2018-01-04 06:20:11 +0000

    🌆📼 Checkpoint bot.js:616740

--- bot.js
+++ bot.js
@@ -259,7 +259,7 @@ if (!process.env.clientId || !process.env.clientSecret) {
       /***** New route submitted *****/
       controller.on('dialog_submission', function(bot, message) {
         var submission = message.submission;
-        bot.reply(message, 'Your route has been created, you will be notified riders wish to carpool');
+        bot.reply(message, 'Your route has been created and you will be notified if riders wish to carpool');
 
         bot.dialogOk();
       });
