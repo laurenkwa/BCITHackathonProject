6201655b11bc03b7547dae45d77443b9e41430da
Glitch (ride-share) <none>
2018-01-04 18:26:38 +0000

    🌊🐩 Checkpoint skills/create_ride.js:616740

--- skills/create_ride.js
+++ skills/create_ride.js
@@ -94,7 +94,7 @@ module.exports = function(controller) {
                           convo.say('Great. I\'ll tell the rideshare channel.');
                           //convo.next();
                           controller.storage.channels.delete(message.user, function(err){
-                            controller.storage.channels.save({id: message.user, name: name, image: thumbnail, driver: '@' + driver, seats:submission.Seats, time:submission.Time, date:submission.Date}, function(err, user) {
+                            controller.storage.channels.save({id: message.user, name: name, image: thumbnail, driver: '<@' + message.user + '>', seats:submission.Seats, time:submission.Time, date:submission.Date}, function(err, user) {
                               controller.storage.channels.get(message.user, function(err, user) {bot.reply(message, 'New route created: ' + user.name); alertChannel(bot, user);});  
                             });
                           });
