0b7049d879b0819325a08ca3dfb2a966cd06efd7
Glitch (ride-share) <none>
2018-01-04 19:17:40 +0000

    🔫🐁 Checkpoint skills/create_ride.js:616740

--- skills/create_ride.js
+++ skills/create_ride.js
@@ -264,7 +264,30 @@ module.exports = function(controller) {
           + '\nSeats: ' + user.seats + '\nLeaving: ' + user.date + " at " + user.time + 
           "\n<" + user.image + "|Map preview>",
         channel: "C8NT4J1C7",
-        attachments: [{
+        attachments: 
+          [{
+            
+            "fallback": "Required plain-text summary of the attachment.",
+            "color": "#36a64f",
+            "pretext": "Optional text that appears above the attachment block",
+            "author_name": "Bobby Tables",
+            "author_link": "http://flickr.com/bobby/",
+            "author_icon": "http://flickr.com/icons/bobby.jpg",
+            "title": "Slack API Documentation",
+            "title_link": "https://api.slack.com/",
+            "text": "Optional text that appears within the attachment",
+            "fields": [
+                {
+                    "title": "Priority",
+                    "value": "High",
+                    "short": false
+                }
+            ],
+            "image_url": "http://my-website.com/path/to/image.jpg",
+            "thumb_url": "http://example.com/path/to/thumb.png",
+            "footer": "Slack API",
+            "footer_icon": "https://platform.slack-edge.com/img/default_application_icon.png",
+            "ts": 123456789
             image_url: user.image
         }]
         });
