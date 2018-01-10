02fb588c74068813625894f8754ffc2340f3c1a2
CRonYii <kenchenrong@gmail.com>
2018-01-09 21:25:32 -0800

    change configuration

--- includes/nav.php
+++ includes/nav.php
@@ -11,7 +11,7 @@
     echo('<p>Welcome to Ride-Share, <span style="font-weight: bold;">' . $_SESSION['user_name'] . '</span>!</p>');
     echo('<p><a href="logout.php" class="btn btn-danger">Log out</a></p>');
   } else {
-    echo('<p><a href="https://slack.com/oauth/authorize?client_id=293788574964.293935676385&amp;scope=identity.basic&amp;redirect_uri=https%3A%2F%2Fride-share.azurewebsites.net%2Fprocess_login.php"><img src="https://platform.slack-edge.com/img/sign_in_with_slack.png" srcset="https://platform.slack-edge.com/img/sign_in_with_slack.png 1x, https://platform.slack-edge.com/img/sign_in_with_slack@2x.png 2x"></a></p>');
+    echo('<p><a href="https://slack.com/oauth/authorize?client_id=155127176102.293670961635&amp;scope=identity.basic&amp;redirect_uri=https%3A%2F%2Fride-share.azurewebsites.net%2Fprocess_login.php"><img src="https://platform.slack-edge.com/img/sign_in_with_slack.png" srcset="https://platform.slack-edge.com/img/sign_in_with_slack.png 1x, https://platform.slack-edge.com/img/sign_in_with_slack@2x.png 2x"></a></p>');
   }
   ?>
 </div>
