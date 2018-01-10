74d25c0fea12ad26b0774d5bfc6bf9c4fee79fd2
CRonYii <kenchenrong@gmail.com>
2018-01-09 22:10:22 -0800

    haha

--- includes/nav.php
+++ includes/nav.php
@@ -5,13 +5,13 @@
   <p><a href="https://ride-share.glitch.me/" target="_blank"><button class = "btn btn-primary" id="slackLink" >Download Slack App</button></a></p>
 
 </div>
-<div class="text-center" style="background-color: #eee;padding: 10px;">
+<div class="text-center" style="background-color: #eee;padding: 10px; margin-bottom: 10px;">
   <?php
   if (isset($_SESSION['access_token'])) {
     echo('<p>Welcome to Ride-Share, <span style="font-weight: bold;">' . $_SESSION['user_name'] . '</span>!</p>');
     echo('<p><a href="logout.php" class="btn btn-danger">Log out</a></p>');
   } else {
-    echo('<p><a href="https://slack.com/oauth/authorize?client_id=293788574964.293935676385&scope=identity.basic&redirect_uri=https://ride-share.azurewebsites.net/process_login.php"><img src="https://platform.slack-edge.com/img/sign_in_with_slack.png" srcset="https://platform.slack-edge.com/img/sign_in_with_slack.png 1x, https://platform.slack-edge.com/img/sign_in_with_slack@2x.png 2x"></a></p>');
+    echo('<p><a href="https://slack.com/oauth/authorize?client_id=155127176102.293670961635&scope=identity.basic&redirect_uri=https://ride-share.azurewebsites.net/process_login.php"><img src="https://platform.slack-edge.com/img/sign_in_with_slack.png" srcset="https://platform.slack-edge.com/img/sign_in_with_slack.png 1x, https://platform.slack-edge.com/img/sign_in_with_slack@2x.png 2x"></a></p>');
   }
   ?>
 </div>
